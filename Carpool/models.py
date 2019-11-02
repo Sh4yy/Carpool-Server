42f83a91763e71d84c07feb43a6e7965f8e616e9
Shayan <shayan@umd.edu>
2019-11-02 16:23:01 -0400

    more on routes/models

--- Carpool/models.py
+++ Carpool/models.py
@@ -1,9 +1,12 @@
 from mongoengine import *
+from enum import Enum
+from secrets import token_hex
 
 
 class User(Document):
 
-    email = StringField(required=True, primary_key=True)
+    id = StringField(unique=True, primary_key=True, default=lambda: token_hex(5))
+    email = StringField(required=True)
     first_name = StringField(required=True)
     last_name = StringField(required=True)
     profile_picture = StringField(required=False)
@@ -27,8 +30,9 @@ class User(Document):
     def find_with_email(cls, email):
         return cls.objects.get(email=email.lower())
 
-    def update_location(self, location):
-        pass
+    def update_location(self, lon, lat):
+        self.location = [lon, lat]
+        self.save()
 
     def send_text(self, text_message):
         pass
@@ -36,9 +40,28 @@ class User(Document):
     def send_push(self, push_data):
         pass
 
+    def make_json(self):
+        json = {
+            "id": self.id,
+            "email": self.email,
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "profile_picture": self.profile_picture,
+            "phone_number": self.phone_number,
+            "location": {
+            }
+        }
+
+        if self.location:
+            json['location']['longitude'] = self.location[0]
+            json['location']['latitude'] = self.location[1]
+
+        return json
+
 
 class RideRequest(Document):
 
+    id = StringField(unique=True, primary_key=True, default=lambda: token_hex(5))
     by_user = ReferenceField(User)
     at_time = FloatField(required=True)
     before_flex = IntField(required=True)
@@ -48,18 +71,196 @@ class RideRequest(Document):
     end = FloatField(required=True)
 
     location = PointField(auto_index=True)
-
-    pickup_addr = StringField()
-    dest_addr = StringField()
+    destination = PointField(auto_index=False)
 
     @classmethod
-    def create(cls, by_user, time, before_flex, after_flex, location, pickup_addr, dest_addr):
+    def create(cls, by_user, time, before_flex, after_flex,
+               location_lon, location_lat, dest_lon, dest_lat):
         req = cls()
         req.by_user = by_user
-        req.location = location
-        req.pickup_addr = pickup_addr
-        req.dest_addr = dest_addr
+        req.location = [location_lon, location_lat]
+        req.destination = [dest_lon, dest_lat]
         req.start = time - before_flex
         req.end = time + after_flex
         req.save()
         return req
+
+    @classmethod
+    def find_for_user(cls, user):
+        return cls.objects(by_user=user)
+
+    @classmethod
+    def find_within(cls, lon, lat, radius):
+        """ radius is in meters """
+        return cls.objects(location__near=[lon, lat], location__max_distance=radius)
+
+    @classmethod
+    def find_within_time(cls, lon, lat, radius, start, end):
+        """ radius is in meters """
+        return cls.objects(location__near=[lon, lat], location__max_distance=radius,
+                           start__qe=start, end__lt=end)
+
+    def make_json(self):
+        json = {
+            "id": self.id,
+            "user": self.by_user.make_json(),
+            "start": self.start,
+            "end": self.end,
+            "location": {
+                "longitude": self.location[0],
+                "latitude": self.location[1]
+            },
+            "destination": {
+                "longitude": self.destination[0],
+                "latitude": self.destination[1]
+            },
+            "before_flex": self.before_flex,
+            "after_flex": self.after_flex
+        }
+
+        return json
+
+
+class Ride(Document):
+
+    id = StringField(unique=True, primary_key=True, default=lambda: token_hex(5))
+    by_user = ReferenceField(User)
+    location = PointField(auto_index=True)
+    destination = PointField(auto_index=False)
+
+    start = FloatField()
+    end = FloatField()
+
+    @classmethod
+    def create(cls, by_user, start, end, location_lon,
+               location_lat, dest_lon, dest_lat):
+        ride = cls()
+        ride.by_user = by_user
+        ride.start = start
+        ride.end = end
+        ride.location = [location_lon, location_lat]
+        ride.destination = [dest_lon, dest_lat]
+        ride.save()
+        return ride
+
+    @classmethod
+    def find_for_user(cls, user):
+        return cls.objects(by_user=user)
+
+    def make_json(self):
+        json = {
+            "id": self.id,
+            "user": self.by_user.make_json(),
+            "start": self.start,
+            "end": self.end,
+            "location": {
+                "longitude": self.location[0],
+                "latitude": self.location[1]
+            },
+            "destination": {
+                "longitude": self.destination[0],
+                "latitude": self.destination[1]
+            },
+        }
+
+        return json
+
+
+class EnumField(object):
+    """
+    A class to register Enum type (from the package enum34) into mongo
+    :param choices: must be of :class:`enum.Enum`: type
+        and will be used as possible choices
+    """
+
+    def __init__(self, enum, *args, **kwargs):
+        self.enum = enum
+        kwargs['choices'] = [choice for choice in enum]
+        super(EnumField, self).__init__(*args, **kwargs)
+
+    def __get_value(self, enum):
+        return enum.value if hasattr(enum, 'value') else enum
+
+    def to_python(self, value):
+        return self.enum(super(EnumField, self).to_python(value))
+
+    def to_mongo(self, value):
+        return self.__get_value(value)
+
+    def prepare_query_value(self, op, value):
+        return super(EnumField, self).prepare_query_value(
+                op, self.__get_value(value))
+
+    def validate(self, value):
+        return super(EnumField, self).validate(self.__get_value(value))
+
+    def _validate(self, value, **kwargs):
+        return super(EnumField, self)._validate(
+                self.enum(self.__get_value(value)), **kwargs)
+
+
+class RideMatchingStatus(Enum):
+
+    pending = "pending"
+    accepted = "accepted"
+    rejected = "rejected"
+
+
+class RideMatching(Document):
+
+    id = StringField(unique=True, primary_key=True, default=lambda: token_hex(5))
+    driver = ReferenceField(User)
+    rider = ReferenceField(User)
+    ride = ReferenceField(Ride)
+    request = ReferenceField(RideRequest)
+    cost = FloatField()
+    status = EnumField(RideMatchingStatus)
+
+    @classmethod
+    def create(cls, driver, rider, ride, request, cost):
+        match = cls()
+        match.status = RideMatchingStatus.pending
+        match.rider = rider
+        match.ride = ride
+        match.request = request
+        match.cost = cost
+        match.driver = driver
+        match.save()
+        return match
+
+    @classmethod
+    def find_with_driver(cls, driver):
+        return cls.objects(driver=driver)
+
+    @classmethod
+    def find_with_rider(cls, rider):
+        return cls.objects(rider=rider)
+
+    @classmethod
+    def find_with_ride(cls, ride):
+        return cls.objects(ride=ride)
+
+    @classmethod
+    def find_with_request(cls, request):
+        return cls.objects(request=request)
+
+    def accept(self):
+        self.status = RideMatchingStatus.accepted
+        self.save()
+
+    def reject(self):
+        self.status = RideMatchingStatus.rejected
+        self.save()
+
+    def make_json(self):
+        json = {
+            "id": self.id,
+            "rider": self.rider.make_json(),
+            "driver": self.driver.make_json(),
+            "ride": self.ride.make_json(),
+            "request": self.request.make_json(),
+            "cost": self.cost,
+            "status": self.status
+        }
+
+        return json
---
+++ Carpool/routes.py
@@ -0,0 +1,178 @@
+from flask import Blueprint, jsonify, abort, request
+from .models import *
+
+mod = Blueprint("routes", __name__)
+
+
+@mod.route('/user/<email>', methods=['GET'])
+def get_user_info(email):
+
+    user = User.find_with_email(email)
+    if not user:
+        return abort(404)
+
+    return jsonify(user.make_json())
+
+
+@mod.route("/user", methods=['POST'])
+def register_user():
+
+    data = request.json
+    first_name = data.get('first_name')
+    last_name = data.get('last_name')
+    email = data.get('email')
+    phone_number = data.get('phone_number')
+    profile_picture = data.get('profile_picture')
+
+    user = User.register(email=email, first_name=first_name,
+                         last_name=last_name, phone_number=phone_number,
+                         profile_picture=profile_picture)
+
+    return jsonify(user.make_json())
+
+
+@mod.route('/user/<email>/location', methods=['UPDATE'])
+def update_user_location(email):
+
+    user = User.find_with_email(email)
+    if not user:
+        return abort(404)
+
+    longitude = request.json['location']['longitude']
+    latitude = request.json['location']['latitude']
+    user.update_location(lon=longitude, lat=latitude)
+
+    return jsonify(user.make_json())
+
+
+@mod.route('/user/<email>/ride/request', methods=['POST'])
+def request_new_ride(email):
+
+    user = User.find_with_email(email)
+    data = request.json
+    if not user:
+        return abort(404)
+
+    time = data.get('time')
+    before_flex = data.get('before_flex')
+    after_flex = data.get('after_flex')
+
+    location_lon = data['location']['longitude']
+    location_lat = data['location']['latitude']
+
+    dest_lon = data['destination']['longitude']
+    dest_lat = data['destination']['latitude']
+
+    req = RideRequest.create(
+        by_user=user, time=time, before_flex=before_flex,
+        after_flex=after_flex, location_lon=location_lon,
+        location_lat=location_lat, dest_lon=dest_lon, dest_lat=dest_lat)
+
+    return jsonify(req.make_json())
+
+
+@mod.route('/user/<email>/ride/request', methods=['GET'])
+def get_ride_requests(email):
+
+    user = User.find_with_email(email)
+    if not user:
+        return abort(404)
+
+    requests = RideRequest.find_for_user(user)
+    return jsonify(list(map(lambda x: x.make_json(), requests)))
+
+
+@mod.route('/user/<email>/ride', methods=['POST'])
+def post_new_ride(email):
+
+    user = User.find_with_email(email)
+    if not user:
+        return abort(404)
+
+    data = request.data
+
+    start = data['start_time']
+    end = data['end_time']
+
+    location_lon = data['location']['longitude']
+    location_lat = data['location']['latitude']
+
+    dest_lon = data['destination']['longitude']
+    dest_lat = data['destination']['latitude']
+
+    ride = Ride.create(
+        by_user=user, location_lon=location_lon, location_lat=location_lat,
+        dest_lon=dest_lon, dest_lat=dest_lat, start=start, end=end
+    )
+
+    return jsonify(ride.make_json())
+
+
+@mod.route('/user/<email>/ride', methods=['GET'])
+def get_user_rides(email):
+
+    user = User.find_with_email(email)
+    if not user:
+        return abort(404)
+
+    rides = Ride.find_for_user(user)
+    return jsonify(list(map(lambda x: x.make_json(), rides)))
+
+
+@mod.route('/user/<email>/ride/<ride_id>/match', methods=['POST'])
+def start_matching_ride(email, ride_id):
+
+    # todo perform matching process
+    # find users in route
+    # create a match
+    # notify users
+    # return matches
+
+    ride = Ride.objects.get(id=ride_id)
+
+
+
+
+@mod.route('/user/<email>/ride/<ride_id>/match', methods=['GET'])
+def get_matching_rides(email, ride_id):
+
+    user = User.find_with_email(email)
+    if not user:
+        return abort(404)
+
+    ride = Ride.objects.get(id=ride_id)
+    matches = RideMatching.find_with_ride(ride)
+    return jsonify(list(map(lambda x: x.make_json(), matches)))
+
+
+@mod.route('/user/<email>/ride/<ride_id>/match/accept', methods=['POST'])
+def accept_ride_match(email, ride_id):
+
+    user = User.find_with_email(email)
+    if not user:
+        return abort(404)
+
+    ride = Ride.objects.get(id=ride_id)
+    ride_match = RideMatching.objects.get(ride=ride, rider=user)
+    if not ride_match:
+        return abort(404)
+
+    ride_match.accept()
+    return jsonify(ride_match.make_json())
+
+
+@mod.route('/user/<email>/ride/<ride_id>/match/reject', methods=['POST'])
+def reject_ride_match(email, ride_id):
+
+    user = User.find_with_email(email)
+    if not user:
+        return abort(404)
+
+    ride = Ride.objects.get(id=ride_id)
+    ride_match = RideMatching.objects.get(ride=ride, rider=user)
+    if not ride_match:
+        return abort(404)
+
+    ride_match.reject()
+    return jsonify(ride_match.make_json())
+
